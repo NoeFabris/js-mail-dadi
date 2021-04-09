545efc14e9ae7737278535462cef9d2b501b3381
NoeFabris <noe@elysresidence.com>
2021-04-09 17:28:11 +0200

    Mail v2

--- Mail/myscript.js
+++ Mail/myscript.js
@@ -1,14 +1,15 @@
 var mail= prompt("Insert your E-mail");
-var listaAccesso = ["noe@secure-web.it", "mail"]
-
+var listaAccesso = ["noe@secure-web.it", "mail", "mail1", "mail2", "mail3"];
+var mailLower = mail.toLowerCase()
+console.log(mailLower)
 for (let index = 0; index < listaAccesso.length; index++) {
     var element = listaAccesso[index];
-    console.log(element)
-    if (mail === listaAccesso[index]) {
+    console.log(element);
+    if (mailLower === listaAccesso[index]) {
         document.getElementById('output').innerHTML= "Yes" ;
+        index = listaAccesso.length;
     } else {
         document.getElementById('output').innerHTML= "No" ;
-
+        console.log(index);
     }
-    
 }
