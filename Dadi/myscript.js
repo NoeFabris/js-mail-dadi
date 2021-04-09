9a0bbbb9db49fb9a89ca0a48581c0e38297199a3
NoeFabris <noe@elysresidence.com>
2021-04-09 17:42:59 +0200

    Es dadi funzionante

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -1,34 +1,13 @@
-var km = prompt("Distance to travel in km");
-var age = prompt("Your Age");
-console.log(km);
-console.log(age);
+var num1 = (Math.floor(Math.random() * 6) + 1)
+var num2 = (Math.floor(Math.random() * 6) + 1)
 
-if ((km > 0) && (age >= 0)) {
-    if (age < 18) {
-        var price = km * 0.21;
-        var price20 = (price - (price * 20 / 100)).toFixed(2);
-        console.log(price20);
-        document.getElementById('price').innerHTML= price20 ;
-    } else {
-        if (age < 65) {
-            var price = km * 0.21;
-            console.log(price);
-            document.getElementById('price').innerHTML= price ;
-        } else {
-            var price = km * 0.21;
-            var price40 = (price - (price * 40 / 100)).toFixed(2);
-            console.log(price40);
-            document.getElementById('price').innerHTML= price40 ;
-        }
-    }
+
+console.log(num1)
+console.log(num2)
+
+
+if (num1 > num2) {
+    document.getElementById("output").innerHTML = "Umano Vince"
 } else {
-    if (isNaN(km) && isNaN(age)) {
-        document.getElementById('price').innerHTML= "Please use numbers" ;
-    } else if (isNaN(age)) {
-        document.getElementById('price').innerHTML= "Please insert AGE in numbers" ;
-    } else {
-        document.getElementById('price').innerHTML= "Please insert KM in numbers" ;
-    }
+    document.getElementById("output").innerHTML = "Computer Vince"
 }
-
-
