3515e5f107c52b4d4b7fc5d4bded7e8f902c540f
NoeFabris <noe@elysresidence.com>
2021-04-09 17:53:31 +0200

    Dadi v3

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -5,7 +5,20 @@ var num2 = (Math.floor(Math.random() * 6) + 1)
 console.log(num1)
 console.log(num2)
 if (num1 === num2) {
-    document.getElementById("output").innerHTML = "Pareggio"
+    while (num1 === num2) {
+        var num1 = (Math.floor(Math.random() * 6) + 1)
+        var num2 = (Math.floor(Math.random() * 6) + 1)
+    
+    
+        console.log(num1)
+        console.log(num2)
+    }
+    if (num1 > num2) {
+        document.getElementById("output").innerHTML = "Umano Vince"
+    } else {
+        document.getElementById("output").innerHTML = "Computer Vince"
+    }
+    // document.getElementById("output").innerHTML = "Pareggio"
 } else {
     if (num1 > num2) {
         document.getElementById("output").innerHTML = "Umano Vince"
